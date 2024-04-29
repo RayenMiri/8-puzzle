37b9b93830d898d8c25ed46b8ce0683ffe37b436
RayenMiri <rayenmiri000@gmail.com>
2024-04-29 23:13:43 +0100

    new changes

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ def swap_tiles(clicked_tile,empty_tile):
     tiles[clicked_tile]['text'],tiles[empty_tile]['text'] = tiles[empty_tile]['text'],tiles[clicked_tile]['text']
     tiles[empty_tile]['background'] = 'white'
     tiles[clicked_tile]['background']=cf._from_rgb((0,255,100))
-           
+
 def on_tile_click(row,col):
     empty_x,empty_y = find_empty_tile()
     if((row == empty_x and abs(col - empty_y) == 1) or (col == empty_y and abs(row - empty_x) == 1)):
@@ -36,12 +36,8 @@ def is_solvable(puzzle):
 
 def check_winning_state():
     current_state = [tiles[(row,col)]['text'] for row in range(3) for col in range(3)]
-    print(current_state)
-    
-
-
-def suffle_tiles():
-    return False
+    if(current_state == [str(i) for i in range(1,9)]+[""]):
+        print('Congratulations , You Won !!')
 
 def generate_puzzle():
     while True:
@@ -51,6 +47,14 @@ def generate_puzzle():
         
 def bot_solver():
     return False
+
+def reset_game():
+    global puzzle
+    puzzle = generate_puzzle()  # Generate a new solvable puzzle
+    tiles[find_empty_tile()]["background"]='white'
+    for c, i in enumerate(puzzle.flatten()):
+        row, col = divmod(c, 3)
+        tiles[(row, col)].config(text=str(i) if i != 0 else "", background=cf._from_rgb((0, 255, 100)) if i == 0 else None)
 
 def start_game(p):
     global tiles
@@ -63,7 +67,8 @@ def start_game(p):
             tile  = tk.Button(mainWindow,background='white',text=str(i),width=7, height=4,command=lambda x=row,y=col : on_tile_click(x,y))
         tile.grid(row=row,column = col)
         tiles[(row,col)] = tile
-    
+    reset_button = tk.Button(mainWindow, text="Restart", width=7, height=2, command=reset_game)
+    reset_button.grid(row=3,column=1)
 if __name__ == '__main__':
     puzzle = generate_puzzle()
     start_game(puzzle)
